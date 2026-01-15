2ebcb9354c5f230687ce2e07706b898169e33b28
Lynn Haydar <lhaydar@c2r5s3.42beirut.com>
2026-01-15 17:12:03 +0200

    Fixed norminette

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 15:03:31 by lhaydar           #+#    #+#             */
-/*   Updated: 2026/01/15 17:08:04 by lhaydar          ###   ########.fr       */
+/*   Updated: 2026/01/15 17:11:17 by lhaydar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
