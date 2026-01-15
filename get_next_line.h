3de109c5b84646aaca7dda3e784d38b32fbd1488
Lynn Haydar <lhaydar@c2r5s3.42beirut.com>
2026-01-15 17:10:17 +0200

    Modified Header File

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 15:03:31 by lhaydar           #+#    #+#             */
-/*   Updated: 2026/01/08 16:21:09 by lhaydar          ###   ########.fr       */
+/*   Updated: 2026/01/15 17:08:04 by lhaydar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd);
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
